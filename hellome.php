894a33225621952d39ef7a3cc7d247b99b704fed
Kirsten Markley <markleyk@otc.edu>
2024-08-28 07:54:27 -0500

    initial commit

---
+++ hellome.php
@@ -0,0 +1,17 @@
+<?php
+
+    // varible for my name
+    $name = "Markley";
+
+    // assoc array with my info
+    $assoc_array = [
+        "degree" => "Teacher",
+        "tran_mode" => "Car",
+        "repo" => "https://github.com/MarkleyClassroom/introtophp"
+    ];
+
+    // display the name in the terminal
+    echo ($name);
+
+    // display the contents of the array
+    print_r($assoc_array);
